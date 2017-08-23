b675218efbd01bf0eb52740d3f79925037b61038
Matt Smith <mattonfoot@gmail.com>
2017-08-23 22:52:31 +0100

    tests for passed offence factor

--- test/OGRS3.spec.js
+++ test/OGRS3.spec.js
@@ -135,5 +135,45 @@ describe('OGRS3 Calculator', function () {
 
       result.OGRS3.result.should.eql([expected1yr, expected2yr]);
     });
+
+    it('should caclulate the same result as the existing tool even for a custom factor', function() {
+      var result = calcOGRS3({
+        coefficients: coefficients,
+
+        gender: 1,                                       // female
+        currentOffenceFactor: 0.204960477,               // Criminal Damage
+        previousSanctions: 3,
+        birthDate: (new Date(1972, 8, 9)),
+        convictionDate: (new Date(2009, 8, 9)),
+        firstSanctionDate: (new Date(1986, 8, 9)),
+        assessmentDate: (new Date(2015, 8, 15)),
+      });
+
+      const ln = (x) =>
+        Math.log(x);
+      const probabilityOfProvenReoffending = (z) =>
+        ((ex) => ex / (1 + ex))(Math.exp(z));
+
+      let b1 = 1.251124464 * ln((3 + 1) / (10 + 37 - 14));
+      let b2 = 0.463062792;
+      let b3 = -1.524652221;
+      let b4 = 0.204960477;
+
+      var expected1yr = probabilityOfProvenReoffending(b3 + b2 + b1 + b4 + 1.402562384);
+      var expected2yr = probabilityOfProvenReoffending(b3 + b2 + b1 + b4 + 2.121705678);
+
+
+      result.should.have.property('OGRS3');
+      result.OGRS3.should.have.property('result');
+      result.OGRS3.should.have.property('explain');
+      console.log(result.OGRS3.explain);
+
+      result.OGRS3.explain.currentOffenceFactor.should.equal(b4);
+      result.OGRS3.explain.agePolynominalFactor.should.equal(b3);
+      result.OGRS3.explain.numberOfSanctionsFactor.should.equal(b2);
+      result.OGRS3.explain.copasRate.should.equal(b1);
+
+      result.OGRS3.result.should.eql([expected1yr, expected2yr]);
+    });
   });
 });
