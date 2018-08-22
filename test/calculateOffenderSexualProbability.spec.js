85c2ced82192d6f428d274496481f8f4a3d84ae0
Matt Smith <mattonfoot@gmail.com>
2018-08-22 14:19:40 +0100

    precaclulate criminal history and known checks

--- test/calculateOffenderSexualProbability.spec.js
+++ test/calculateOffenderSexualProbability.spec.js
@@ -24,8 +24,7 @@ describe('Offender Sexual Predictor Score', () => {
 
   describe('when only data is that there is no current or previous sexual element', () => {
     var data = {
-      sexualOffenceHistory: 1,
-      sexualElement: 1,
+      hasSexualElementOrOffence: false,
     };
 
     it('should produce two scores of 0', () => {
@@ -35,7 +34,8 @@ describe('Offender Sexual Predictor Score', () => {
 
   describe('when data describes a woman', () => {
     var data = {
-      sex: 1,
+      isFemale: true,
+      isMale: false,
     };
 
     it('should produce two scores of 0', () => {
@@ -45,19 +45,9 @@ describe('Offender Sexual Predictor Score', () => {
 
   describe('when data describes a woman with sexual history', () => {
     var data = {
-      sex: 1,
-      sexualOffenceHistory: 0,
-    };
-
-    it('should produce two scores of 0', () => {
-      calc(data).should.eql([0.0051813471502590676, 0.0051813471502590676]);
-    });
-  });
-
-  describe('when data describes a woman with a sexual element', () => {
-    var data = {
-      sex: 1,
-      sexualElement: 0,
+      isFemale: true,
+      isMale: false,
+      hasSexualElementOrOffence: true,
     };
 
     it('should produce two scores of 0', () => {
@@ -67,12 +57,12 @@ describe('Offender Sexual Predictor Score', () => {
 
   describe('when data describes a male with one previous sexual offence', () => {
     var data = {
-      sex: 0,                             // 0 = Male, 1 = Female
-      sexualOffenceHistory: 1,            // 0 = yes, 1 = no
-      sexualElement: 0,                   // 0 = yes, 1 = no
-      birthDate: new Date(1974, 0, 1),
-      mostRecentSexualOffence: new Date(1998, 0, 1),
-      sentenceDate: new Date(2017, 0, 1),
+      isFemale: false,
+      isMale: true,
+      hasSexualElementOrOffence: true,
+
+      ageAtSentenceDate: 44,
+      ageAtMostRecentSexualOffence: 24,
     };
 
     it('should produce the correct two scores', () => {
@@ -82,14 +72,12 @@ describe('Offender Sexual Predictor Score', () => {
 
   describe('when data describes a 20yr old male sex offender', () => {
     var data = {
-      sex: 0,                             // 0 = Male, 1 = Female
-      sexualOffenceHistory: 1,            // 0 = yes, 1 = no
-      sexualElement: 0,                   // 0 = yes, 1 = no
-      birthDate: new Date(1997, 0, 1),
-      convictionDate: new Date(2017, 0, 1),
-      mostRecentSexualOffence: new Date(2017, 0, 1),
-      assessmentDate: new Date(2017, 0, 1),
-      sentenceDate: new Date(2017, 0, 1),
+      isFemale: false,
+      isMale: true,
+      hasSexualElementOrOffence: true,
+
+      ageAtSentenceDate: 20,
+      ageAtMostRecentSexualOffence: 20,
     };
 
     it('should produce the correct two scores', () => {
@@ -99,14 +87,12 @@ describe('Offender Sexual Predictor Score', () => {
 
   describe('when data describes a 30yr old male sex offender', () => {
     var data = {
-      sex: 0,                             // 0 = Male, 1 = Female
-      sexualOffenceHistory: 0,            // 0 = yes, 1 = no
-      sexualElement: 0,                   // 0 = yes, 1 = no
-      birthDate: new Date(1987, 0, 1),
-      convictionDate: new Date(2017, 0, 1),
-      mostRecentSexualOffence: new Date(2017, 0, 1),
-      assessmentDate: new Date(2017, 0, 1),
-      sentenceDate: new Date(2017, 0, 1),
+      isFemale: false,
+      isMale: true,
+      hasSexualElementOrOffence: true,
+
+      ageAtSentenceDate: 30,
+      ageAtMostRecentSexualOffence: 30,
     };
 
     it('should produce the correct two scores', () => {
@@ -116,14 +102,12 @@ describe('Offender Sexual Predictor Score', () => {
 
   describe('when data describes a 60yr old male sex offender', () => {
     var data = {
-      sex: 0,                             // 0 = Male, 1 = Female
-      sexualOffenceHistory: 0,            // 0 = yes, 1 = no
-      sexualElement: 1,                   // 0 = yes, 1 = no
-      birthDate: new Date(1957, 0, 1),
-      convictionDate: new Date(2017, 0, 1),
-      mostRecentSexualOffence: new Date(2017, 0, 1),
-      assessmentDate: new Date(2017, 0, 1),
-      sentenceDate: new Date(2017, 0, 1),
+      isFemale: false,
+      isMale: true,
+      hasSexualElementOrOffence: true,
+
+      ageAtSentenceDate: 60,
+      ageAtMostRecentSexualOffence: 60,
     };
 
     it('should produce the correct two scores', () => {
